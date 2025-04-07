4ac1b17a8f31f1d5f38d1cc5c03c84e40219b32f
Pablo <pmacsal2707@g.educaand.es>
2025-04-07 14:49:12 +0200

    codigo

---
+++ PHP/inventario/inc/navbar.php
@@ -0,0 +1,64 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="">
+      <img src="./img/logo.png" alt="" width="65" height="28">
+  </a>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">Usuarios</a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">Nuevo</a>
+          <a class="navbar-item">Lista</a>
+          <a class="navbar-item">Buscar</a>
+        </div>
+      </div>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">Categorías</a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">Nueva</a>
+          <a class="navbar-item">Lista</a>
+          <a class="navbar-item">Buscar</a>
+        </div>
+      </div>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">Productos</a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">Nuevo</a>
+          <a class="navbar-item">Lista</a>
+          <a class="navbar-item">Por categorías</a>
+          <a class="navbar-item">Buscar</a>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary is-rounded">
+            Mi cuenta
+          </a>
+          <a class="button is-dark is-rounded">
+            Salir
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
---
+++ PHP/inventario/index.php
@@ -0,0 +1,14 @@
+<?php require "./inc/session_start.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "./inc/head.php"; ?>
+</head>
+<body>
+    <?php
+        include "./inc/navbar.php";
+        
+        include "./inc/script.php";
+    ?>
+</body>
+</html>
